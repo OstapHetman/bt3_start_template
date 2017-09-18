d5dda16425be62585e8714a2ba31b8a0bfa7cad5
OstapHetman <prickly4528@gmail.com>
2017-09-18 11:53:37 +0300

    implemet wait task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 let gulp = require('gulp');
 let sass = require('gulp-sass');
+let wait = require('gulp-wait');
 
 gulp.task('sass', function() {
     return gulp.src('sass/**/*.scss')
+        .pipe(wait(150))
         .pipe(sass())
         .pipe(gulp.dest('./css'));
 });
